61b1d7c460c6a7d782734f308bef61773ecff137
Rex Cadiao <jcadiao1102@gmail.com>
2022-04-05 20:43:54 +0800

    Push WebApi .NET 6

---
+++ RestaurantApi/Migrations/20220404155835_Initial.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodItems",
+                columns: table => new
+                {
+                    FoodItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FoodItemName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodItems", x => x.FoodItemId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderMasters",
+                columns: table => new
+                {
+                    OrderMasterId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    PMethod = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderMasters", x => x.OrderMasterId);
+                    table.ForeignKey(
+                        name: "FK_OrderMasters_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderMasterDetails",
+                columns: table => new
+                {
+                    OrderMasterDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderMasterId = table.Column<int>(type: "int", nullable: false),
+                    FoodItemId = table.Column<int>(type: "int", nullable: false),
+                    FoodItemPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderMasterDetails", x => x.OrderMasterDetailId);
+                    table.ForeignKey(
+                        name: "FK_OrderMasterDetails_FoodItems_FoodItemId",
+                        column: x => x.FoodItemId,
+                        principalTable: "FoodItems",
+                        principalColumn: "FoodItemId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderMasterDetails_OrderMasters_OrderMasterId",
+                        column: x => x.OrderMasterId,
+                        principalTable: "OrderMasters",
+                        principalColumn: "OrderMasterId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderMasterDetails_FoodItemId",
+                table: "OrderMasterDetails",
+                column: "FoodItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderMasterDetails_OrderMasterId",
+                table: "OrderMasterDetails",
+                column: "OrderMasterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderMasters_CustomerId",
+                table: "OrderMasters",
+                column: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderMasterDetails");
+
+            migrationBuilder.DropTable(
+                name: "FoodItems");
+
+            migrationBuilder.DropTable(
+                name: "OrderMasters");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
